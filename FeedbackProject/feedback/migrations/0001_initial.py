3da8725b81ed325f6b68ff838614c0c364da8396
Emily Williamson <williamson.emily1@gmail.com>
2014-06-13 15:55:08 -0400

    redoing DB for project

--- FeedbackProject/feedback/migrations/0001_initial.py
+++ FeedbackProject/feedback/migrations/0001_initial.py
@@ -17,10 +17,50 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'feedback', ['BaseEmployee'])
 
+        # Adding model 'FeedbackRecipient'
+        db.create_table(u'feedback_feedbackrecipient', (
+            (u'baseemployee_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['feedback.BaseEmployee'], unique=True, primary_key=True)),
+            ('coach', self.gf('django.db.models.fields.related.ForeignKey')(default='This Fool does not yeat have a feedback coach', related_name='RecipientCoach', to=orm['feedback.BaseEmployee'])),
+        ))
+        db.send_create_signal(u'feedback', ['FeedbackRecipient'])
+
+        # Adding model 'FeedbackGiver'
+        db.create_table(u'feedback_feedbackgiver', (
+            (u'baseemployee_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['feedback.BaseEmployee'], unique=True, primary_key=True)),
+            ('can_quote_giver', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('can_use_giver_name', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('summarize_only', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('coach_chat_before_delivery', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('best_judgement', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'feedback', ['FeedbackGiver'])
+
+        # Adding model 'Feedback'
+        db.create_table(u'feedback_feedback', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date_given', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('feedback', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('recipient', self.gf('django.db.models.fields.related.ForeignKey')(default='', to=orm['feedback.FeedbackRecipient'])),
+            ('giver', self.gf('django.db.models.fields.related.ForeignKey')(default='', to=orm['feedback.FeedbackGiver'])),
+            ('fool_numerical_recommendation', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+            ('top_of_game_description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('next_level', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'feedback', ['Feedback'])
+
 
     def backwards(self, orm):
         # Deleting model 'BaseEmployee'
         db.delete_table(u'feedback_baseemployee')
+
+        # Deleting model 'FeedbackRecipient'
+        db.delete_table(u'feedback_feedbackrecipient')
+
+        # Deleting model 'FeedbackGiver'
+        db.delete_table(u'feedback_feedbackgiver')
+
+        # Deleting model 'Feedback'
+        db.delete_table(u'feedback_feedback')
 
 
     models = {
@@ -30,6 +70,31 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_coach': ('django.db.models.fields.BooleanField', [], {}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        },
+        u'feedback.feedback': {
+            'Meta': {'object_name': 'Feedback'},
+            'date_given': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'feedback': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'fool_numerical_recommendation': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'giver': ('django.db.models.fields.related.ForeignKey', [], {'default': "''", 'to': u"orm['feedback.FeedbackGiver']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'next_level': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'recipient': ('django.db.models.fields.related.ForeignKey', [], {'default': "''", 'to': u"orm['feedback.FeedbackRecipient']"}),
+            'top_of_game_description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'feedback.feedbackgiver': {
+            'Meta': {'object_name': 'FeedbackGiver', '_ormbases': [u'feedback.BaseEmployee']},
+            u'baseemployee_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['feedback.BaseEmployee']", 'unique': 'True', 'primary_key': 'True'}),
+            'best_judgement': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'can_quote_giver': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'can_use_giver_name': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'coach_chat_before_delivery': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'summarize_only': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'feedback.feedbackrecipient': {
+            'Meta': {'object_name': 'FeedbackRecipient', '_ormbases': [u'feedback.BaseEmployee']},
+            u'baseemployee_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['feedback.BaseEmployee']", 'unique': 'True', 'primary_key': 'True'}),
+            'coach': ('django.db.models.fields.related.ForeignKey', [], {'default': "'This Fool does not yeat have a feedback coach'", 'related_name': "'RecipientCoach'", 'to': u"orm['feedback.BaseEmployee']"})
         }
     }
 
--- FeedbackProject/feedback/models.py
+++ FeedbackProject/feedback/models.py
@@ -1,20 +1,43 @@
 from django.db import models
 
+NUMERICAL_SCALE = (
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5),
+	(6, 6),
+	(7, 7),
+	(8, 8),
+	(9, 9),
+	(10,10),
+)
+
 class BaseEmployee(models.Model):
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
 	is_coach = models.BooleanField()
 
 
-class FeedbackRecipient(models.Model):
-	coach = models.ForeignKey(BaseEmployee, default="This Fool does not yeat have a feedback coach", limit_choices_to={'is_coach': True})
+class FeedbackRecipient(BaseEmployee):
+	coach = models.ForeignKey(BaseEmployee, default="This Fool does not yeat have a feedback coach", 
+		limit_choices_to={'is_coach': True}, related_name='RecipientCoach'
+	)
+
+
+class FeedbackGiver(BaseEmployee):
+	can_quote_giver = models.BooleanField(default=False)
+	can_use_giver_name = models.BooleanField(default=False)
+	summarize_only = models.BooleanField(default=False)
+	coach_chat_before_delivery = models.BooleanField(default=False)
+	best_judgement = models.BooleanField(default=False)
 
 
 class Feedback(models.Model):
 	date_given = models.DateTimeField(auto_now=True, blank=True)
 	feedback = models.TextField(null=True, blank=True)
 	recipient = models.ForeignKey(FeedbackRecipient, default="")
-	
-
-class FeedbackGiver(models.Model):
-	feedback_to_give = models.ForeignKey(Feedback, null=True, blank = True)
+	giver = models.ForeignKey(FeedbackGiver, default="")
+	fool_numerical_recommendation = models.PositiveIntegerField(choices=NUMERICAL_SCALE, null=True, blank=True)
+	top_of_game_description = models.TextField(null=True, blank=True)
+	next_level = models.TextField(null=True, blank=True)
